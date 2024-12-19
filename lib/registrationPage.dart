1b7668702b3514ec5d3edcfda0ed4e4b4458f1eb
defisently <112123664+Defisently@users.noreply.github.com>
2024-12-19 11:47:31 +0300

    pageDesign

--- lib/registrationPage.dart
+++ lib/registrationPage.dart
@@ -20,52 +20,62 @@ class _RegistrationPageState extends State<RegistrationPage> {
       "rightsLevel": 1,
     };
 
-
-    final response = await http.post(Uri.parse('https://localhost:5000/newUser'), body: json.encode(params));
-    Map<String, dynamic> data = jsonDecode(response.body) as Map<String, dynamic>;
-
+    final response = await http.post(
+        Uri.parse('https://localhost:5000/newUser'),
+        body: json.encode(params));
+    Map<String, dynamic> data =
+        jsonDecode(response.body) as Map<String, dynamic>;
   }
-
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
         title: const Text("Registration"),
-
       ),
       body: Center(
-
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text("Username"),
+            const Text(
+              "Username",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
             TextFormField(
               controller: usernameController,
               decoration: const InputDecoration(
-                  border: UnderlineInputBorder(),),
+                border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 1.0)),
+              ),
             ),
-            const Text("Password"),
+            const Text("Password",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                )),
             TextFormField(
               controller: passwordController,
               decoration: const InputDecoration(
-                border: UnderlineInputBorder(),),
+                border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey, width: 1.0)),
+              ),
             ),
+            ElevatedButton(onPressed: registerUser, child: const Text("Save")),
             ElevatedButton(
-                onPressed: registerUser,
-                child: const Text("Save")
-            ),
-
-
-
+              onPressed: registerUser,
+              child: const Align(
+                alignment: Alignment.bottomRight,
+                child: Text("Registration for admin",
+                    style:
+                        TextStyle(fontSize: 15, fontWeight: FontWeight.w600)),
+              ),
+            )
           ],
         ),
       ),
-
-
     );
   }
 }
