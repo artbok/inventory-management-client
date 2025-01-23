a9e09fe3278fa56c192d957dfa9f5ab61c21d496
defisently <maksoid8763@gmail.com>
2025-01-23 15:17:02 +0300

    admin items requests

--- lib/requests/getItemsRequests.dart
+++ lib/requests/getItemsRequests.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../localStorage.dart';
 
-Future<Map<String, dynamic>> getItemsRequests(String owner) async {
+Future<Map<String, dynamic>> getItemsRequests([String? owner]) async {
   String? username = getValue("username");
   String? password = getValue("password");
   Map<String, dynamic> params = {
