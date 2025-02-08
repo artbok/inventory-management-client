5ec7fbb93452f62b4797152693fcac884e645c04
Temjik <artem.bokans@gmail.com>
2025-02-08 16:35:35 +0300

    server url

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:inventory_managment/redirect_page.dart';
-
-//must be deleted!
 import 'package:hive_flutter/adapters.dart';
 import 'package:inventory_managment/local_storage.dart';
 
 const String serverAddress = "http://127.0.0.1:5000";
+
+String getServer() {
+  return const String.fromEnvironment('SERVER_URL', defaultValue: serverAddress);
+}
+
 void main() async {
-  //must be deleted!
+  String server = getServer();
   await Hive.initFlutter();
   await Hive.openBox("storage");
-  putToTheStorage("serverAddress", serverAddress);
-  putToTheStorage("username", "bebrobruh");
-  //putToTheStorage("username", "bebra");
-  putToTheStorage("password", "12345");
+  putToTheStorage("serverAddress", server);
+  // putToTheStorage("username", "bebrobruh");
+  // putToTheStorage("username", "bebra");
+  // putToTheStorage("password", "12345");
 
   runApp(const RedirectPage());
 }
